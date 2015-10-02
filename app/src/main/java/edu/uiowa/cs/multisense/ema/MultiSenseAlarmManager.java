463375c206ed0473ee78d443702398595d89f9cc
syedshabihhasan <hasanshabih@gmail.com>
2015-10-02 00:35:08 -0500

    the alarm intent now carries the current timestamp for filenaming

--- app/src/main/java/edu/uiowa/cs/multisense/ema/MultiSenseAlarmManager.java
+++ app/src/main/java/edu/uiowa/cs/multisense/ema/MultiSenseAlarmManager.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import java.util.Calendar;
+
 import edu.uiowa.cs.multisense.MultiSense;
 import edu.uiowa.cs.multisense.sensors.RecordAudioEMA;
 
@@ -30,33 +32,51 @@ public class MultiSenseAlarmManager extends BroadcastReceiver {
         if(1 == res){
             if(intent.getAction().equals("StartActivity")){
                 Log.d("MS:", "Has StartActivity extra");
+                String currentDT = intent.getStringExtra("CurrentDT");
 
                 intent = new Intent(context, MultiSense.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 intent.setAction("SurveyAlarm");
-
+                intent.putExtra("CurrentDT", currentDT);
                 //before starting survey, set the alarm for the next audio
                 int surveyRand = setMultiSenseAlarms.getNextSurveyTime(true);
-                setMultiSenseAlarms.setAlarm(surveyRand*60*1000, true);
+                setMultiSenseAlarms.setAlarm(surveyRand*60*1000, true, "");
 
                 context.startActivity(intent);
             }
             else if(intent.getAction().equals("StartAudio")){
                 Log.d("MS:", "Has StartAudio extra");
                 //before starting the audio recording, set the alarm for the survey
-                //TODO: do something here about the 1
-                setMultiSenseAlarms.setAlarm(1*60*1000, false);
+
+                String currentDT = getCurrentDateTime();
                 intent = new Intent(context, RecordAudioEMA.class);
+                intent.putExtra("CurrentDT", currentDT);
+
+                //TODO: do something here about the 1
+                setMultiSenseAlarms.setAlarm(1 * 60 * 1000, false, currentDT);
 
                 context.startService(intent);
             }
         }else if(0 == res){
             int surveyRand = setMultiSenseAlarms.getNextSurveyTime(true);
-            setMultiSenseAlarms.setAlarm(surveyRand*60*1000, true);
+            setMultiSenseAlarms.setAlarm(surveyRand*60*1000, true, "");
 
         }else{
             Log.d("MS:", "No more alarms, bye!");
             // the end date and time have passed, do nothing
         }
+    }
+
+    public String getCurrentDateTime(){
+        Calendar calendar = Calendar.getInstance();
+        String currentTimeDate = ""+
+                calendar.get(Calendar.YEAR)+"_"+
+                (calendar.get(Calendar.MONTH)+1)+"_"+
+                calendar.get(Calendar.DAY_OF_MONTH)+"_"+
+                calendar.get(Calendar.HOUR_OF_DAY)+"_"+
+                calendar.get(Calendar.MINUTE)+"_"+
+                calendar.get(Calendar.SECOND)+"_"+
+                calendar.get(Calendar.MILLISECOND);
+        return currentTimeDate;
     }
 }
