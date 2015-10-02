f014eb9f685e4b5c0a09240a239867f58f5326fe
syedshabihhasan <hasanshabih@gmail.com>
2015-10-02 00:34:35 -0500

    data store now also writes to file

--- app/src/main/java/edu/uiowa/cs/multisense/fileio/DataStore.java
+++ app/src/main/java/edu/uiowa/cs/multisense/fileio/DataStore.java
@@ -1,5 +1,10 @@
 package edu.uiowa.cs.multisense.fileio;
 
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 import edu.uiowa.cs.multisense.MultiSenseConstants;
@@ -14,13 +19,39 @@ public class DataStore {
 
     private ArrayList<int[]> surveyResp;
     private String timeStamp;
+    private String allResponses;
     private int DataType;
-
 
     public DataStore(ArrayList<int[]> surveyResp, String timeStamp){
         this.surveyResp = surveyResp;
         this.timeStamp = timeStamp;
         this.DataType = MultiSenseConstants.SURVEY_TYPE;
+        this.allResponses = "";
+    }
+
+    public void WriteSurveyToFile(boolean timeOut){
+        Log.d("MS:", "Writing survey to file");
+        int[] temp;
+        for(int i=0; i< this.surveyResp.size(); i++){
+            temp = this.surveyResp.get(i);
+            this.allResponses += "" + temp[0] + ", " + temp[1] + "\n";
+        }
+        if(timeOut){
+            this.allResponses += "TIMEOUT";
+        }
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(
+                    MultiSenseConstants.FILE_LOCATION + this.timeStamp + ".survey", false);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+            outputStreamWriter.write(this.allResponses);
+            outputStreamWriter.close();
+            fileOutputStream.close();
+            Log.d("MS:", "Written survey to file");
+        }catch(Exception e){
+            Log.d("MS:", "An error occurred while writing survey to file");
+            e.printStackTrace();
+        }
+
     }
 
 
