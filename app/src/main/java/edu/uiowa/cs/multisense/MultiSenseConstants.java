d6e8d1c994520ba931a896c2ca291662ae933ede
syedshabihhasan <hasanshabih@gmail.com>
2015-10-02 00:33:20 -0500

    added some constants

--- app/src/main/java/edu/uiowa/cs/multisense/MultiSenseConstants.java
+++ app/src/main/java/edu/uiowa/cs/multisense/MultiSenseConstants.java
@@ -19,6 +19,8 @@ public class MultiSenseConstants {
     public final static String FILE_LOCATION = "/sdcard/MultiSense/";
 
     public static final int TIMER_COUNT = 2*60*1000;
+
+
     public static final String[] MONTH_DICT = new String[]{
             "JAN",
             "FEB",
@@ -34,9 +36,23 @@ public class MultiSenseConstants {
             "DEC"
     };
 
-    public static final float EDITTEXT_FONTSIZE = 32;
+    public static final float EDITTEXT_FONTSIZE = 24;
     public static final float TEXTVIEW_FONTSIZE = 32;
-    public static final float BUTTON_FONTSIZE = 32;
+    public static final float BUTTON_FONTSIZE = 24;
 
     public static final String CONFIG_FILENAME = "multisenseConfig.cfg";
+    public static final String INTERNAL_SETTINGS_FILENAME = "multisenseInternals.int";
+
+    public static final int PID = 0;
+    public static final int DID = 1;
+    public static final int SURVEY_INTERVAL = 2;
+    public static final int RANDOM_INTERVAL = 3;
+    public static final int SURVEY_TIMEOUT = 4;
+    public static final int MAINSCREEN_TIMEOUT = 5;
+    public static final int START_DATE = 6;
+    public static final int END_DATE = 7;
+    public static final int START_TIME = 8;
+    public static final int END_TIME = 9;
+
+    public static final long BUTTON_VIBRATE_DURATION = 500;
 }
